1d3cbabb23470c1d7b7a817c97cc423e43732dc7
kippesp <kippesp@5570acc8-2f4d-42b0-af27-b08595442e6d>
2012-02-26 20:26:46 +0000

    record all beacons
    git-svn-id: https://subversion.assembla.com/svn/gtaiad/trunk@12 5570acc8-2f4d-42b0-af27-b08595442e6d

--- cataloger/aircrack-ng-1.1.gatech/script.sh
+++ cataloger/aircrack-ng-1.1.gatech/script.sh
@@ -2,4 +2,4 @@ file_date=$(date +%m%d%H%M)
 mkdir advanced_internet_files
 mkdir advanced_internet_files/$file_date'nishith'
 #printf '%s' $file_date
-./src/airodump-ng -f 1000 -w advanced_internet_files/$file_date'nishith'/$file_date --channel 1,6,11 wlan0
+./src/airodump-ng -f 2000 -w advanced_internet_files/$file_date'nishith'/$file_date --channel 1,6,11 --beacons wlan0
