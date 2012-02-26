1ca1ddd99e3e98ca9bc98eb27675a70037b5dfe7
kippesp <kippesp@5570acc8-2f4d-42b0-af27-b08595442e6d>
2012-02-26 20:51:21 +0000

    script update
    git-svn-id: https://subversion.assembla.com/svn/gtaiad/trunk@15 5570acc8-2f4d-42b0-af27-b08595442e6d

--- cataloger/aircrack-ng-1.1.gatech/script.sh
+++ cataloger/aircrack-ng-1.1.gatech/script.sh
@@ -6,4 +6,4 @@ read LOCTAG
 DPATH=advanced_internet_files/$(date +%m%d)-$UNAME/$file_date-$UNAME-$LOCTAG
 mkdir -p $DPATH
 #printf '%s' $file_date
-echo ./src/airodump-ng -f 20000 -w $DPATH/$LOCTAG --channel 1,6,11 --beacons wlan0
+./src/airodump-ng -f 20000 --write $DPATH/$UNAME-$LOCTAG --channel 1,6,11 --beacons wlan1
