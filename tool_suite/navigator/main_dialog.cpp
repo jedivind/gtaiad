2d108ae3a50e3ae3525c4039c5a78e243c40a95e
n3nash <n3nash@5570acc8-2f4d-42b0-af27-b08595442e6d>
2012-03-20 16:02:29 +0000

    git-svn-id: https://subversion.assembla.com/svn/gtaiad/trunk@78 5570acc8-2f4d-42b0-af27-b08595442e6d

--- tool_suite/navigator/main_dialog.cpp
+++ tool_suite/navigator/main_dialog.cpp
@@ -50,7 +50,7 @@ void MainDialog::update_floor_scale(int scaling_factor)
 
 void MainDialog::update_location_clicked(void)
 {
-  if (x_pos_label->text().isEmpty() && y_pos_label->text().isEmpty())
+  if (x_pos_label->text().isEmpty() && y_pos_label->text().isEmpty())   //what if the user presses update again. The marker is not cleared.
   {
     //call code to run airodump and get the present location data.feed it into lily's code.
     //call lily's code and other map refreshing code.
@@ -59,6 +59,16 @@ void MainDialog::update_location_clicked(void)
     QMessageBox::warning(this, "Location",
         "Updating for the first time.");
     return;
+  }
+  else
+  {
+	//clear marker.
+    calculate_position_from_data(get_measurement_locations);
+    update_location_changed();
+    QMessageBox::warning(this, "Location",
+        "Updating for the first time.");
+    return;
+
   }
 
   if (!validate_loc_id())
@@ -73,33 +83,11 @@ void MainDialog::update_location_clicked(void)
 }
 
 // slot: called when user clicks on the floor map for red dot placement
-void MainDialog::update_location_changed(const QPointF& pos)
-{
   // If the location was updated and x and y have some value but now after some time
   // presses the update again .
   // TODO: Update enables of buttons when loc ID is entered
-    MapScene* map_scene;
-
-    map_scene = (MapScene*)(map_view->scene());
-    map_scene->clear_marker_placement();
-
-  QPoint int_pos = pos.toPoint();  // does proper rounding
-
-  x_pos_label->setText(QString::number(int_pos.x()));
-  y_pos_label->setText(QString::number(int_pos.y()));
-}
 
 // Do necessary clearing of widgets and enable/disables.need to design appropriate widgets first.
-/*void MainDialog::exit_capture_mode(void)
-{
-  x_pos_label->clear();
-  y_pos_label->clear();
-  new_capture_groupbox->setEnabled(false);
-  new_capture_push_button->setEnabled(true);
-  floor_combobox->setEnabled(true);
-
-  emit new_capture_canceled();
-}*/
 
 // Generate the MapScene object for each floor map.  Store the scenes in
 // the QList<T> generic container.
@@ -120,41 +108,6 @@ void MainDialog::init_floor_scenes(void)
   }
 }
 
-// read from the database all of a floor's loc_ids and xpos,ypos information
-QList< QPair<QString, QPointF> > MainDialog::get_measurement_locations(int floor_number)
-{
-  QSqlQuery q(m_db);
-  QList< QPair<QString, QPointF> > rvalue;
-
-  q.prepare(" \
-      SELECT \
-        capture_location_name, capture_location_x_pos, capture_location_y_pos \
-      FROM capture_locations \
-      WHERE capture_location_floor = :capture_location_floor");
-
-  q.bindValue(":capture_location_floor", floor_number);
-
-  bool res = q.exec();
-
-  if (!res)
-  {
-    QMessageBox::warning(this, "Database Error",
-        "Failed during SELECT from capture_locations table.");
-    return rvalue;
-  }
-
-  while (q.next())
-  {
-    QPair<QString, QPointF> tuple;
-
-    tuple.first = q.value(0).toString();
-    tuple.second = QPointF(q.value(1).toFloat(), q.value(2).toFloat());
-
-    rvalue.append(tuple);
-  }
-
-  return rvalue;
-}
 
 
 // slot: called when floor is changed.that is when the user clicks update_position again then refresh the floor if it has changed.
